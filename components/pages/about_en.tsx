a7cf683510138566c61a65b71377bca71ef7abb5
Luis Gonzalez <luis.gonzalez@airbnb.com>
2021-10-19 17:29:13 -0700

    About complete

--- components/pages/about_en.tsx
+++ components/pages/about_en.tsx
@@ -1,9 +1,86 @@
+import LinkText from '../LinkText';
 import PageLangLayout from './PageLangLayout';
 
 const AboutEn = () => (
   <PageLangLayout>
-    <div className="md:w-1/2 lg:w-1/3">
-      <p className="mt-3 text-2xl">About</p>
+    <div className="md:w-1/2 lg:w-1/2">
+      <h1 className="mt-3 text-2xl">About me</h1>
+      <p className="mt-4">
+        I&apos;m originally from Zacatecas, Mexico, I graduated from{' '}
+        <LinkText href="https://zacatecas.tecnm.mx" target="_blank" underline>
+          Instituto Tecnológico de Zacatecas
+        </LinkText>{' '}
+        with a bachelor&apos;s in computer science with a major in Software
+        Engineering. I&apos;ve been working in the field for 10+ years.
+      </p>
+      <p className="mt-4">
+        My goal was always to be able to work at the top companies in the
+        industry and I&apos;ve been fortunate enough to join some of them. (for
+        more resume details please visit my{' '}
+        <LinkText
+          href="https://www.linkedin.com/in/luismgonzalezb/"
+          underline
+          target="_blank"
+        >
+          LinkedIn
+        </LinkText>
+        )
+      </p>
+      <p className="mt-4">
+        Even before joining my college degree I had tremendous passion for
+        software, my first programming language was{' '}
+        <LinkText
+          href="https://el.media.mit.edu/logo-foundation/index.html"
+          target="_blank"
+          underline
+        >
+          Logo.
+        </LinkText>{' '}
+        I also learned Pascal and ASP, after that .Net and finally Web, mainly
+        doing Javascript.
+      </p>
+      <p className="mt-4">
+        I also really enjoy playing video games, currently my main game is{' '}
+        <LinkText
+          href="https://www.ea.com/games/apex-legends"
+          target="_blank"
+          underline
+        >
+          Apex Legends
+        </LinkText>{' '}
+        (a free to play game I thoroughly recommend), in general I really like
+        the shooter genre, but I also really enjoy games like{' '}
+        <LinkText
+          href="https://www.ageofempires.com/"
+          target="_blank"
+          underline
+        >
+          Age of Empires.
+        </LinkText>
+      </p>
+      <p className="mt-4">
+        The music I like the most is Metal, my faves are Metallica, Disturbed,
+        System of a Down, Lamb of God, etc, thankfully I&apos;ve seen them all
+        live and if you can you should totally see your favs also live,
+        it&apos;s really special.
+      </p>
+      <p className="mt-4">
+        Also I&apos;m a father of 2 girls, so we always do what the ladies say
+        at home, that didn&apos;t happened before, my wife is quite an
+        accomplished leader, I owe to her being able to cease some of the
+        opportunities that have come my way, the best formula for growth is to
+        have a partner that supports and encourages you. We moved to near
+        Seattle, WA about 1 year ago and we&apos;ve enjoyed quite a lot around
+        here.
+      </p>
+      <p className="mt-4">
+        Finally something I&apos;ve always loved is to be able to help people
+        that might be interested in entering or growing in the Software
+        Development field, unfortunately I think latin@s we are severely
+        underrepresented specially on the top companies and I&apos;m sure is not
+        a lack of talent but just the need of a bit more preparation and have
+        someone to lend you a hand, so if you are interested please contact me.
+      </p>
     </div>
   </PageLangLayout>
 );
--- components/pages/about_es.tsx
+++ components/pages/about_es.tsx
@@ -1,12 +1,86 @@
+import LinkText from '../LinkText';
 import PageLangLayout from './PageLangLayout';
 
 const AboutEs = () => (
   <PageLangLayout>
-    <div className="md:w-1/2 lg:w-1/3">
-      <p className="mt-3 text-2xl">Acerca de mi</p>
-      <p>
-        Originario de Zacatecas, Mexico, estudie la carrera de Ingenieria de
-        Software, he trabajado en el campo de Ingenieria de software
+    <div className="md:w-1/2 lg:w-1/2">
+      <h1 className="mt-3 text-2xl">Acerca de mi</h1>
+      <p className="mt-4">
+        Originario de Zacatecas, México, estudié la carrera de Ingeniería de
+        Software en el{' '}
+        <LinkText href="https://zacatecas.tecnm.mx" target="_blank" underline>
+          Instituto Tecnológico de Zacatecas
+        </LinkText>{' '}
+        y he trabajado en el área de Software por más de 10 años.
+      </p>
+      <p className="mt-4">
+        Mi meta siempre fue poder trabajar dentro de las empresas líderes en el
+        ramo, y he tenido la fortuna de poder entrar a algunas de ellas. (Si
+        gustas mas detalles sobre mi carrera ve mi{' '}
+        <LinkText
+          href="https://www.linkedin.com/in/luismgonzalezb/"
+          underline
+          target="_blank"
+        >
+          LinkedIn
+        </LinkText>
+        ).
+      </p>
+      <p className="mt-4">
+        Desde antes de la carrera siempre tuve una pasión por el desarrollo, mi
+        primer lenguaje fue{' '}
+        <LinkText
+          href="https://el.media.mit.edu/logo-foundation/index.html"
+          target="_blank"
+          underline
+        >
+          Logo
+        </LinkText>{' '}
+        en la secundaria, aprendí Pascal y ASP después trabajé en .Net y
+        finalmente Web, principalmente en Javascript.
+      </p>
+      <p className="mt-4">
+        Otra cosa que me encanta son los videojuegos, actualmente el que más
+        juego es{' '}
+        <LinkText
+          href="https://www.ea.com/games/apex-legends"
+          target="_blank"
+          underline
+        >
+          Apex Legends
+        </LinkText>{' '}
+        (un juego gratuito que te recomiendo), en general me gusta el género de
+        “shooters” pero también me gustan mucho los de estrategia como{' '}
+        <LinkText
+          href="https://www.ageofempires.com/"
+          target="_blank"
+          underline
+        >
+          Age of Empires
+        </LinkText>
+      </p>
+      <p className="mt-4">
+        Mi música predilecta es Metal, mis bandas favoritas son Metallica,
+        Disturbed, System of A Down, Lamb of God, etc. Afortunadamente he tenido
+        la fortuna de verlos a todos en vivo y si es algo que le recomendaría a
+        cualquiera.
+      </p>
+      <p className="mt-4">
+        Soy también padre de familia, dos niñas, así que siempre en mi casa se
+        hará lo que las mujeres quieran, no que antes de que ellas llegaran no,
+        mi esposa es una mujer sumamente capaz con un gran liderazgo, a ella le
+        debo mucho de las oportunidades que he podido capturar, no hay mejor
+        fórmula que una pareja que te apoye y te impulse. Nos mudamos hace 1 año
+        a Seattle, WA, y hemos disfrutado mucho el área.
+      </p>
+      <p className="mt-4">
+        Finalmente algo que siempre me ha gustado es ayudar a las personas que
+        estén interesadas en entrar o crecer en la ingeniería en sistemas y el
+        desarrollo de software, creo que desafortunadamente los latin@s estamos
+        muy poco representados en software en las empresas de más alto nivel y
+        estoy seguro que no es falta de talento, es solo un poco mas de
+        preparacion y alguien que te dé un empujoncito, así que si tu eres una
+        persona interesada por favor no dudes en ponerte en contacto.
       </p>
     </div>
   </PageLangLayout>
