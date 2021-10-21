2e8756ff0159ab0c83ad5d8c00124c15dad016f6
Luis Gonzalez <luismgonzalezb@gmail.com>
2021-10-20 21:54:28 -0700

    Fixed images and text wrap around picture on about

--- components/pages/about_en.tsx
+++ components/pages/about_en.tsx
@@ -1,25 +1,25 @@
-import Image from 'next/image';
-import LinkText from '../LinkText';
-import PageLangLayout from './PageLangLayout';
-import desk from '../../public/img/desk.jpeg';
-import fall from '../../public/img/fall.jpeg';
+import Image from "next/image";
+import LinkText from "../LinkText";
+import PageLangLayout from "./PageLangLayout";
+import desk from "../../public/img/desk.jpeg";
+import fall from "../../public/img/fall.jpeg";
 
 const AboutEn = () => (
   <PageLangLayout>
     <div className="md:w-1/2 lg:w-1/2">
       <h1 className="mt-3 text-2xl">About me</h1>
       <p className="mt-4">
-        I&apos;m originally from Zacatecas, Mexico, I graduated from{' '}
+        I&apos;m originally from Zacatecas, Mexico, I graduated from{" "}
         <LinkText href="https://zacatecas.tecnm.mx" target="_blank" underline>
           Instituto Tecnológico de Zacatecas
-        </LinkText>{' '}
+        </LinkText>{" "}
         with a bachelor&apos;s in computer science with a major in Software
         Engineering. I&apos;ve been working in the field for 10+ years.
       </p>
       <p className="mt-4">
         My goal was always to be able to work at the top companies in the
         industry and I&apos;ve been fortunate enough to join some of them. (for
-        more resume details please visit my{' '}
+        more resume details please visit my{" "}
         <LinkText
           href="https://www.linkedin.com/in/luismgonzalezb/"
           underline
@@ -29,33 +29,33 @@ const AboutEn = () => (
         </LinkText>
         )
       </p>
-      <div className="mt-4">
+      <div className="mt-4 flex justify-center">
         <Image src={desk} alt="Picture of my current desk at home" />
       </div>
       <p className="mt-4">
         Even before joining my college degree I had tremendous passion for
-        software, my first programming language was{' '}
+        software, my first programming language was{" "}
         <LinkText
           href="https://el.media.mit.edu/logo-foundation/index.html"
           target="_blank"
           underline
         >
           Logo.
-        </LinkText>{' '}
+        </LinkText>{" "}
         I also learned Pascal and ASP, after that .Net and finally Web, mainly
         doing Javascript.
       </p>
       <p className="mt-4">
-        I also really enjoy playing video games, currently my main game is{' '}
+        I also really enjoy playing video games, currently my main game is{" "}
         <LinkText
           href="https://www.ea.com/games/apex-legends"
           target="_blank"
           underline
         >
           Apex Legends
-        </LinkText>{' '}
+        </LinkText>{" "}
         (a free to play game I thoroughly recommend), in general I really like
-        the shooter genre, but I also really enjoy games like{' '}
+        the shooter genre, but I also really enjoy games like{" "}
         <LinkText
           href="https://www.ageofempires.com/"
           target="_blank"
@@ -70,8 +70,8 @@ const AboutEn = () => (
         live and if you can you should totally see your favs also live,
         it&apos;s really special.
       </p>
-      <div className="flex flex-wrap mt-4">
-        <div className="w-full md:w-1/2 pr-4">
+      <div className="mt-4">
+        <div className="w-full md:w-1/2 pr-4 float-left">
           <Image src={fall} alt="Picture of me and my girl walking" />
         </div>
         <p className="w-full md:w-1/2">
@@ -83,15 +83,16 @@ const AboutEn = () => (
           Seattle, WA about 1 year ago and we&apos;ve enjoyed quite a lot around
           here.
         </p>
+        <p className="mt-4">
+          Finally something I&apos;ve always loved is to be able to help people
+          that might be interested in entering or growing in the Software
+          Development field, unfortunately I think latin@s we are severely
+          underrepresented specially on the top companies and I&apos;m sure is
+          not a lack of talent but just the need of a bit more preparation and
+          have someone to lend you a hand, so if you are interested please
+          contact me.
+        </p>
       </div>
-      <p className="mt-4">
-        Finally something I&apos;ve always loved is to be able to help people
-        that might be interested in entering or growing in the Software
-        Development field, unfortunately I think latin@s we are severely
-        underrepresented specially on the top companies and I&apos;m sure is not
-        a lack of talent but just the need of a bit more preparation and have
-        someone to lend you a hand, so if you are interested please contact me.
-      </p>
     </div>
   </PageLangLayout>
 );
--- components/pages/about_es.tsx
+++ components/pages/about_es.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image';
-import LinkText from '../LinkText';
-import PageLangLayout from './PageLangLayout';
-import desk from '../../public/img/desk.jpeg';
-import fall from '../../public/img/fall.jpeg';
+import Image from "next/image";
+import LinkText from "../LinkText";
+import PageLangLayout from "./PageLangLayout";
+import desk from "../../public/img/desk.jpeg";
+import fall from "../../public/img/fall.jpeg";
 
 const AboutEs = () => (
   <PageLangLayout>
@@ -10,16 +10,16 @@ const AboutEs = () => (
       <h1 className="mt-3 text-2xl">Acerca de mi</h1>
       <p className="mt-4">
         Originario de Zacatecas, México, estudié la carrera de Ingeniería de
-        Software en el{' '}
+        Software en el{" "}
         <LinkText href="https://zacatecas.tecnm.mx" target="_blank" underline>
           Instituto Tecnológico de Zacatecas
-        </LinkText>{' '}
+        </LinkText>{" "}
         y he trabajado en el área de Software por más de 10 años.
       </p>
       <p className="mt-4">
         Mi meta siempre fue poder trabajar dentro de las empresas líderes en el
         ramo, y he tenido la fortuna de poder entrar a algunas de ellas. (Si
-        gustas mas detalles sobre mi carrera ve mi{' '}
+        gustas mas detalles sobre mi carrera ve mi{" "}
         <LinkText
           href="https://www.linkedin.com/in/luismgonzalezb/"
           underline
@@ -29,34 +29,34 @@ const AboutEs = () => (
         </LinkText>
         ).
       </p>
-      <div className="mt-4">
+      <div className="mt-4 flex justify-center">
         <Image src={desk} alt="Picture of my current desk at home" />
       </div>
       <p className="mt-4">
         Desde antes de la carrera siempre tuve una pasión por el desarrollo, mi
-        primer lenguaje fue{' '}
+        primer lenguaje fue{" "}
         <LinkText
           href="https://el.media.mit.edu/logo-foundation/index.html"
           target="_blank"
           underline
         >
           Logo
-        </LinkText>{' '}
+        </LinkText>{" "}
         en la secundaria, aprendí Pascal y ASP después trabajé en .Net y
         finalmente Web, principalmente en Javascript.
       </p>
       <p className="mt-4">
         Otra cosa que me encanta son los videojuegos, actualmente el que más
-        juego es{' '}
+        juego es{" "}
         <LinkText
           href="https://www.ea.com/games/apex-legends"
           target="_blank"
           underline
         >
           Apex Legends
-        </LinkText>{' '}
+        </LinkText>{" "}
         (un juego gratuito que te recomiendo), en general me gusta el género de
-        “shooters” pero también me gustan mucho los de estrategia como{' '}
+        “shooters” pero también me gustan mucho los de estrategia como{" "}
         <LinkText
           href="https://www.ageofempires.com/"
           target="_blank"
@@ -71,11 +71,11 @@ const AboutEs = () => (
         la fortuna de verlos a todos en vivo y si es algo que le recomendaría a
         cualquiera.
       </p>
-      <div className="flex flex-wrap mt-4">
-        <div className="w-full md:w-1/2 pr-4">
+      <div className="mt-4">
+        <div className="w-full md:w-1/2 pr-4 float-left">
           <Image src={fall} alt="Picture of me and my girl walking" />
         </div>
-        <p className="w-full md:w-1/2">
+        <p>
           Soy también padre de familia, dos niñas, así que siempre en mi casa se
           hará lo que las mujeres quieran, no que antes de que ellas llegaran
           no, mi esposa es una mujer sumamente capaz con un gran liderazgo, a
@@ -83,16 +83,16 @@ const AboutEs = () => (
           mejor fórmula que una pareja que te apoye y te impulse. Nos mudamos
           hace 1 año a Seattle, WA, y hemos disfrutado mucho el área.
         </p>
+        <p className="mt-4">
+          Finalmente algo que siempre me ha gustado es ayudar a las personas que
+          estén interesadas en entrar o crecer en la ingeniería en sistemas y el
+          desarrollo de software, creo que desafortunadamente los latin@s
+          estamos muy poco representados en software en las empresas de más alto
+          nivel y estoy seguro que no es falta de talento, es solo un poco mas
+          de preparacion y alguien que te dé un empujoncito, así que si tu eres
+          una persona interesada por favor no dudes en ponerte en contacto.
+        </p>
       </div>
-      <p className="mt-4">
-        Finalmente algo que siempre me ha gustado es ayudar a las personas que
-        estén interesadas en entrar o crecer en la ingeniería en sistemas y el
-        desarrollo de software, creo que desafortunadamente los latin@s estamos
-        muy poco representados en software en las empresas de más alto nivel y
-        estoy seguro que no es falta de talento, es solo un poco mas de
-        preparacion y alguien que te dé un empujoncito, así que si tu eres una
-        persona interesada por favor no dudes en ponerte en contacto.
-      </p>
     </div>
   </PageLangLayout>
 );
